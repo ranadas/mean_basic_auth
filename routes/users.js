fedfc08ee68e9deee2b2ca870c3622d63066deeb
x148128 <rana.das@hrblock.com>
2016-11-21 13:43:56 +0000

    adding mongo db - WIP

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
+
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  
+  var cursor = db.collection('accounts').find()
+  console.log(cursor);
+  res.send('getting all  User Accounts..');
 });
 
 module.exports = router;
