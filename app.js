2f9dfb86523dbe452cfbe3cef7069c653ce3c20a
x148128 <rana.das@hrblock.com>
2016-11-21 15:52:33 +0000

    adding index which has account add  feature

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// add mongoose & passport to be used in auth
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 var index = require('./routes/index');
 var users = require('./routes/users');
+var ping_service = require('./routes/ping-service');
 
 var app = express();
 
@@ -19,10 +25,32 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// add session handling and passport
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
+// TODO 1. move out of jade, when  all done. . 
+//app.use('htmls/', express.static(path.join(__dirname, 'htmls')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/ping', ping_service);
+
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose
+mongoose.connect('mongodb://localhost/passport_local_mongoose_express4');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
 var express = require('express');
+const passport = require('passport');
+const Account = require('../models/account');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Auth Project !!!' });
+  // res.render('index', { title: 'Auth Project !!!' });
+  res.render('index', { user : req.user });
 });
+
+/* GET register page. */
+router.get('/register', function(req, res) {
+    res.render('register', { });
+});
+
+router.post('/register', function(req, res) {
+    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+        if (err) {
+            return res.render('register', { account : account });
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            res.redirect('/');
+        });
+    });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res) {
+    res.render('login', { user : req.user });
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+});
+
+/* GET logout page, "/". */
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
 
 module.exports = router;
