b8827ceee55eea3a4300cb1f467e46ff2ff92717
x148128 <rana.das@hrblock.com>
2016-11-21 15:01:31 +0000

    returning the select * from accounts in the get call http://localhost:3000/users/

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,30 @@
+"use strict";
 var express = require('express');
-
 var mongoose = require('mongoose');
-var db = mongoose.connection;
-
+//var db = mongoose.connection;
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  
-  var cursor = db.collection('accounts').find()
-  console.log(cursor);
-  res.send('getting all  User Accounts..');
+    getAllAccouns(res);
+    // res.send("Some response");
 });
+
+//retrieve all accouns (model registered in accounts.js) from MonogoDb, and write to the response object
+function getAllAccouns(response) {
+    mongoose.model('accounts').find({}, function (err, results) {
+        if (err) {
+            return console.error(err);
+        } else {
+            console.log("RETURNING : -> " + results);
+            response.send(results);
+            //return results;
+        }
+    });
+    // var cursor = db.collection('accounts').find()
+    // db.collection('accounts').find().toArray(function(err, results) {
+    //   console.log("\n-->"+results)
+    // });
+}
 
 module.exports = router;
