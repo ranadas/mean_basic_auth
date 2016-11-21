7fd0e16fae7c317f5a3ec894c09f00191c611df7
x148128 <rana.das@hrblock.com>
2016-11-21 16:32:34 +0000

    adding search

--- routes/users.js
+++ routes/users.js
@@ -35,20 +35,40 @@ function getAllAccouns(response) {
 }
 
 function findById(response, idToSearch) {
-    //TODO 
+    // get a account with ID of 1
+    mongoose.model('accounts').findById(idToSearch, function(err, user) {
+        if (err) throw err;
+        console.log(user);
+        response.send(results);
+    });
 }
 
 function findByName(nameToSearch, response) {
     // todo : check if nameToSearch or empty
-    console.log("Searching for " + nameToSearch);    
+    console.log("Searching for " + nameToSearch);
     mongoose.model('accounts').find({ username: nameToSearch }, function(err, user) {
-        if (err) 
+        if (err)
             throw err;
-        
+
         console.log(user);
         response.send(user);
     });
 }
 
+function find(response, searchString) {
+    // TODO get any admin that was created in the past month
 
+    // get the date 1 month ago
+    var monthAgo = new Date();
+    monthAgo.setMonth(monthAgo.getMonth() - 1);
+
+    mongoose.model('accounts').find({ admin: true }).where('created_at').gt(monthAgo).exec(function(err, users) {
+        if (err) throw err;
+
+        // show the admins in the past month
+        console.log(users);
+    });
+}
+
+//https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
 module.exports = router;
